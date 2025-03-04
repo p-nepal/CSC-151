24b9115bf9a4585ab07136d0af614dd236825208
Pranay Nepal <nepalp5175@student.faytechcc.edu>
2025-03-04 14:45:45 -0500

    Add files via upload
    Debugging code for java

---
+++ Bugged_01_NepalPranay/src/bugged_01_nepalpranay/Bugged_01_NepalPranay.java
@@ -0,0 +1,114 @@
+
+package bugged_01_nepalpranay;
+
+
+import java.util.Scanner;
+
+public class Bugged_01_NepalPranay
+{
+   public static void main(String[] args) 
+     { 
+      final double GUEST_PRICE = 35.00;
+      double price;
+      int guests;
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter number of guests >> ");
+      guests = input.nextInt();
+      System.out.println("***************************************************");
+      System.out.println("*                                                 *");
+      System.out.println("*        Jim's food makes the party great.        *");
+      System.out.println("*                                                 *");
+      System.out.println("***************************************************");
+      price = guests * GUEST_PRICE;
+      System.out.println("The price for an event with " + guests + " guests at $" + GUEST_PRICE + " per guest is $" + price);
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+HEY PRANAY
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
